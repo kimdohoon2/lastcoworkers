b649cfd5114fed4987f8a58110f708f61ab1eb6f
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-17 20:26:40 +0900

    feat : 폰트, 컬러, 폰트사이즈, line-height 적용이 되었는지 코드 추가 - feature 브랜치로 merge할시 삭제하고 올리겠습니다!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,74 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="space-y-4 p-4">
+      {/* Brand Colors */}
+      <div className="bg-brand-primary p-4 text-white">Brand Primary</div>
+      <div className="bg-brand-secondary p-4 text-white">Brand Secondary</div>
+      <div className="bg-brand-tertiary p-4 text-white">Brand Tertiary</div>
+      <div className="from-brand-primary to-brand-tertiary bg-gradient-to-r p-4 text-white">
+        Brand Gradient
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      {/* Point Colors */}
+      <div className="bg-point-purple p-4 text-4xl font-semibold text-white">
+        Point Purple
+      </div>
+      <div className="bg-point-blue p-4 text-white">Point Blue</div>
+      <div className="bg-point-cyan p-4 text-white">Point Cyan</div>
+      <div className="bg-point-pink p-4 text-white">Point Pink</div>
+      <div className="bg-point-rose p-4 text-white">Point Rose</div>
+      <div className="bg-point-orange p-4 text-white">Point Orange</div>
+      <div className="bg-point-yellow p-4 text-white">Point Yellow</div>
+      <div className="bg-point-red p-4 text-white">Point Red</div>
+
+      {/* Background Colors */}
+      <div className="bg-background-primary p-4 text-white">
+        Background Primary
+      </div>
+      <div className="bg-background-secondary p-4 text-white">
+        Background Secondary
+      </div>
+      <div className="bg-background-tertiary p-4 text-white">
+        Background Tertiary
+      </div>
+      <div className="bg-background-inverse p-4 text-black">
+        Background Inverse
+      </div>
+
+      {/* Interaction Colors */}
+      <div className="bg-interaction-inactive p-4 text-white">
+        Interaction Inactive
+      </div>
+      <div className="bg-interaction-hover p-4 text-white">
+        Interaction Hover
+      </div>
+      <div className="bg-interaction-pressed p-4 text-white">
+        Interaction Pressed
+      </div>
+      <div className="bg-interaction-focus p-4 text-white">
+        Interaction Focus
+      </div>
+
+      {/* Border Colors */}
+      <div className="border-border-primary border-4 p-4">Border Primary</div>
+
+      {/* Text Colors */}
+      <div className="text-text-primary p-4">Text Primary</div>
+      <div className="text-text-secondary p-4">Text Secondary</div>
+      <div className="text-text-tertiary p-4">Text Tertiary</div>
+      <div className="text-text-danger p-4">Text Danger</div>
+      <div className="text-text-default p-4">Text Default</div>
+      <div className="text-text-inverse p-4">Text Inverse</div>
+      <div className="text-text-disabled p-4">Text Disabled</div>
+
+      {/* Status Colors */}
+      <div className="bg-status-danger p-4 text-white">Status Danger</div>
+
+      {/* Icon Colors */}
+      <div className="text-icon-primary p-4">Icon Primary</div>
+      <div className="text-icon-inverse p-4">Icon Inverse</div>
+      <div className="text-icon-brand p-4">Icon Brand</div>
+      <div className="text-icon-danger inter p-4">Icon Danger</div>
     </div>
   );
 }
