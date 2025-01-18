3e3d2a8d2fda8c1a4f835be73b047301452f9a19
ksm <hpk5802@github.com>
2025-01-18 22:56:34 +0900

    refactor: 모달 외부 클릭 이벤트 Ref로 수정

--- src/app/components/common/modal/Modal.tsx
+++ src/app/components/common/modal/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import IconClose from '@/app/components/icons/IconClose';
 
@@ -17,22 +17,33 @@ function Modal({
   children,
 }: PropsWithChildren<ModalProps>) {
   const [isMounted, setIsMounted] = useState(false);
+  const modalRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     setIsMounted(true);
-  }, []);
+
+    // 모달 외부 클릭 시 닫히도록 이벤트 처리
+    const handleClickOutside = (event: MouseEvent) => {
+      if (modalRef.current && modalRef.current.contains(event.target as Node)) {
+        closeModal();
+      }
+    };
+
+    // 마운트 시 이벤트 리스너 추가
+    document.addEventListener('mousedown', handleClickOutside);
+
+    // clean-up : 언마운트 시 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [closeModal]);
 
   if (!isMounted) return null;
 
   return createPortal(
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center max-[744px]:justify-end">
-      <div
-        className="absolute inset-0 bg-black opacity-50"
-        onClick={closeModal}
-      />
-      <div
-        className={`bg-background-secondary relative flex w-96 flex-col items-center rounded-xl pb-8 pt-12 max-[744px]:w-full max-[744px]:rounded-b-none`}
-      >
+      <div ref={modalRef} className="absolute inset-0 bg-black opacity-50" />
+      <div className="relative flex w-96 flex-col items-center rounded-xl bg-background-secondary pb-8 pt-12 max-[744px]:w-full max-[744px]:rounded-b-none">
         {hasCloseBtn && (
           <button
             type="button"
