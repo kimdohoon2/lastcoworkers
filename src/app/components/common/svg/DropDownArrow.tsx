e01482e5e12db12ed04a677fc35ffcb669021618
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-18 16:48:46 +0900

    feat : Header에서 사용하는 아이콘 컴퍼넌트 추가완료

---
+++ src/app/components/common/svg/DropDownArrow.tsx
@@ -0,0 +1,19 @@
+export default function DropDownArrow() {
+  return (
+    <svg
+      width="10"
+      height="6"
+      viewBox="0 0 10 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1L5 5L9 1"
+        stroke="#F8FAFC"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
