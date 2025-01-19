70611a8914a53c92f9e484f47c385eebb5f92837
홍지윤 <h_zhirun@naver.com>
2025-01-19 22:22:35 +0900

    style: 드롭다운 아이템 스타일 수정

--- src/app/components/common/dropdown/DropdownItem.tsx
+++ src/app/components/common/dropdown/DropdownItem.tsx
@@ -1,24 +1,27 @@
 import { ReactNode } from 'react';
 
 interface DropdownItemProps {
+  className?: string;
   children: ReactNode;
   onClick: () => void;
   onClose: () => void;
 }
 
 export default function DropdownItem({
+  className,
   children,
   onClick,
   onClose,
 }: DropdownItemProps) {
   return (
-    <li className="list-none whitespace-nowrap rounded-xl px-14 py-10 text-md font-normal text-text-primary">
+    <li className="list-none">
       <button
         onClick={(e) => {
           e.stopPropagation();
           onClick();
           onClose();
         }}
+        className={`w-full whitespace-nowrap rounded-xl px-4 py-3 text-center text-md font-normal text-text-primary hover:bg-background-tertiary ${className}`}
       >
         {children}
       </button>
