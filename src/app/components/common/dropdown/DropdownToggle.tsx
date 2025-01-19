9721d261a232e4cb550139e1aaaf9fca7c97efd7
홍지윤 <h_zhirun@naver.com>
2025-01-19 22:18:01 +0900

    feat: 드롭다운 토글 구현

--- src/app/components/common/dropdown/DropdownToggle.tsx
+++ src/app/components/common/dropdown/DropdownToggle.tsx
@@ -1,5 +1,19 @@
-// 드롭다운을 열고 닫는 토글 버튼
+import { ReactNode } from 'react';
 
-export default function DropdownToggle() {
-  return <div>DropdownToggle</div>;
+interface DropdownToggleProps {
+  className?: string;
+  children: ReactNode;
+  onClick: () => void; // 드롭다운 열고 닫는 함수
+}
+
+export default function DropdownToggle({
+  className,
+  children,
+  onClick,
+}: DropdownToggleProps) {
+  return (
+    <button type="button" className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
