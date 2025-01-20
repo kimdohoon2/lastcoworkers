843b616b241f1a8dec2e3d9f41cf46703d4685ba
JunHee <z1top159@gmail.com>
2025-01-20 15:42:04 +0900

    style: secondary bg 수정

--- src/app/components/common/button/Button.tsx
+++ src/app/components/common/button/Button.tsx
@@ -18,33 +18,32 @@ function Button({
   onClick,
   disabled = false,
 }: ButtonProps) {
-  const baseStyles =
-    'items-center font-semibold text-text-default justify-center rounded-xl';
+  const baseStyles = 'items-center font-semibold justify-center rounded-xl';
 
   const variantStyles = {
     primary:
-      'bg-brand-primary hover:bg-interaction-hover active:bg-interaction-pressed',
+      'bg-brand-primary text-text-default hover:bg-interaction-hover active:bg-interaction-pressed',
     inverse:
       size === 'small'
-        ? 'bg-none border-brand-primary border-[1px] text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed'
+        ? 'bg-background-secondary border-brand-primary border-[1px] text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed'
         : 'bg-background-inverse border-brand-primary border-[1px] text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed',
     secondary:
       'bg-background-inverse text-text-danger border-[1px] border-text-secondary',
-    danger: 'bg-point-red hover:bg-red-600',
+    danger: 'bg-point-red hover:bg-red-600 active:bg-red-700',
   };
 
   const disabledStyles = {
     primary: 'bg-interaction-inactive cursor-not-allowed',
     inverse:
       size === 'small'
-        ? 'bg-none border-[1px] border-interaction-inactive text-interaction-inactive cursor-not-allowed'
+        ? 'bg-background-secondary border-[1px] border-interaction-inactive text-interaction-inactive cursor-not-allowed'
         : 'bg-background-inverse border-[1px] border-interaction-inactive text-interaction-inactive cursor-not-allowed',
     secondary: 'cursor-not-allowed',
     danger: 'bg-gray-200 text-gray-400 cursor-not-allowed',
   };
 
   const sizeStyles = {
-    small: 'px-3 py-2 text-md w-[74px] h-[32px]',
+    small: 'px-2 py-1 text-md w-20 h-[32px]',
     large: 'px-32 py-3.5 text-lg w-[332px] h-12',
   };
 
