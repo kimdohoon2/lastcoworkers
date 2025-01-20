a501b9345a90b7aebce4c5cec9244e777a7676a7
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-20 12:48:28 +0900

    feat : 반응형 작업완료
    - 로그인하고 프로필보이는 로직은 로그인관련 페이지 작업완료후 추가예정
    - 기존 피그마에는 없지만 오른쪽 상단에 로그인으로 바로가는 text추가
    - 자유게시판 text는 로그인하며 보이게끔 로그인관련 페이지 작업완료후 로직 추가 예정

---
+++ src/app/components/common/Header/Header.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import SideMenuBar from '@/app/components/common/Header/SideMenubar';
+import HeaderMenuBar from '@/app/components/icons/HeaderMenuBar';
+import HeaderBoardButton from '@/app/components/common/Header/Boards';
+
+export default function Header() {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const handleOpenSlideMenubar = (): void => setVisible(true);
+  const handleCloseSlideMenubar = (): void => setVisible(false);
+
+  return (
+    <>
+      <header className="fixed left-0 top-0 z-40 w-full bg-background-secondary px-4 py-5 md:px-6 xl:px-[22.5rem] xl:py-3">
+        <div className="flex items-center justify-between">
+          {/* Left Section */}
+          <div className="flex items-center gap-4 md:gap-8 xl:gap-10">
+            <button
+              className="md:hidden"
+              onClick={handleOpenSlideMenubar}
+              aria-label="Open Menu"
+            >
+              <HeaderMenuBar />
+            </button>
+            <div className="h-5 w-[6.3rem] xl:h-8 xl:w-[9.8rem]">
+              <Link className="block h-full w-full" href="/">
+                <Image
+                  className="object-contain"
+                  src="/logos/HeaderLogo.png"
+                  alt="logo"
+                  width={158}
+                  height={32}
+                />
+              </Link>
+            </div>
+            {/* 자유게시판 버튼 */}
+            <HeaderBoardButton className="hidden md:block" />
+          </div>
+          <div>
+            <Link
+              className="text-md hover:text-interaction-hover xl:text-base"
+              href="/login"
+            >
+              로그인
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      {/* 사이드 메뉴 바 */}
+      <SideMenuBar visible={visible} onClose={handleCloseSlideMenubar} />
+    </>
+  );
+}
