17854432d929b477db2f32bce8128f1a2e0ec606
JunHee <z1top159@gmail.com>
2025-01-20 15:30:37 +0900

    feat: 버튼 공통 컴포넌트 추가

---
+++ src/app/(component)/button/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import React from 'react';
+import Button from '@/app/components/common/button/Button';
+
+function TestPage() {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    /** 이벤트 리스너 */
+    console.log('Button clicked:', e);
+  };
+
+  return (
+    <div className="flex items-start gap-5 p-8">
+      <div className="flex gap-4">
+        <div className="flex flex-col gap-4">
+          <Button variant="primary" size="large" onClick={handleClick}>
+            생성하기
+          </Button>
+          <Button variant="primary" size="large" disabled>
+            생성하기
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <Button variant="inverse" size="large" onClick={handleClick}>
+            생성하기
+          </Button>
+          <Button variant="inverse" size="large" disabled>
+            생성하기
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <Button variant="secondary" size="large" onClick={handleClick}>
+            생성하기
+          </Button>
+          <Button variant="danger" size="large" onClick={handleClick}>
+            생성하기
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="flex flex-col gap-4">
+          <Button variant="primary" size="small" onClick={handleClick}>
+            생성하기
+          </Button>
+          <Button variant="primary" size="small" disabled>
+            생성하기
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <Button variant="inverse" size="small" onClick={handleClick}>
+            생성하기
+          </Button>
+          <Button variant="inverse" size="small" disabled>
+            생성하기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestPage;
---
+++ src/app/components/common/button/Button.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React from 'react';
+import clsx from 'clsx';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  variant?: 'primary' | 'inverse' | 'secondary' | 'danger';
+  size?: 'small' | 'large';
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+}
+
+function Button({
+  children,
+  variant = 'primary',
+  size = 'large',
+  onClick,
+  disabled = false,
+}: ButtonProps) {
+  const baseStyles =
+    'items-center font-semibold text-text-default justify-center rounded-xl';
+
+  const variantStyles = {
+    primary:
+      'bg-brand-primary hover:bg-interaction-hover active:bg-interaction-pressed',
+    inverse:
+      size === 'small'
+        ? 'bg-none border-brand-primary border-[1px] text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed'
+        : 'bg-background-inverse border-brand-primary border-[1px] text-brand-primary hover:border-interaction-hover hover:text-interaction-hover active:border-interaction-pressed active:text-interaction-pressed',
+    secondary:
+      'bg-background-inverse text-text-danger border-[1px] border-text-secondary',
+    danger: 'bg-point-red hover:bg-red-600',
+  };
+
+  const disabledStyles = {
+    primary: 'bg-interaction-inactive cursor-not-allowed',
+    inverse:
+      size === 'small'
+        ? 'bg-none border-[1px] border-interaction-inactive text-interaction-inactive cursor-not-allowed'
+        : 'bg-background-inverse border-[1px] border-interaction-inactive text-interaction-inactive cursor-not-allowed',
+    secondary: 'cursor-not-allowed',
+    danger: 'bg-gray-200 text-gray-400 cursor-not-allowed',
+  };
+
+  const sizeStyles = {
+    small: 'px-3 py-2 text-md w-[74px] h-[32px]',
+    large: 'px-32 py-3.5 text-lg w-[332px] h-12',
+  };
+
+  return (
+    <button
+      className={clsx(baseStyles, sizeStyles[size], {
+        [variantStyles[variant]]: !disabled,
+        [disabledStyles[variant]]: disabled,
+      })}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
