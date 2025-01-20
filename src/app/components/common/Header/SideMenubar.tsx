19909324c8c76263e9da252c6db2984302da0b0a
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-20 12:40:22 +0900

    feat : 모바일에서 보이는 사이드바 키보드 이벤트 처리, 애니메이션 조건부 랜더링으로 처리완료
    - 닫기 부분 close icon 컴퍼넌트 받아와서 쓸예정

---
+++ src/app/components/common/Header/SideMenubar.tsx
@@ -0,0 +1,54 @@
+import { useRef, useEffect } from 'react';
+import HeaderBoardButton from '@/app/components/common/Header/Boards';
+
+interface SlidemenuBarType {
+  visible: boolean;
+  onClose: () => void;
+}
+
+export default function SideMenuBar({ visible, onClose }: SlidemenuBarType) {
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  // 키보드 이벤트 처리
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (divRef.current) {
+      divRef.current.focus();
+    }
+  }, [visible]);
+
+  return (
+    <div
+      ref={divRef}
+      role="menubar"
+      tabIndex={0}
+      style={{ backgroundColor: 'rgba(15, 23, 42, 0.5)' }}
+      className={`fixed left-0 top-0 z-50 h-screen w-full transition-opacity duration-300 ease-in-out md:hidden ${
+        visible ? 'opacity-100' : 'pointer-events-none opacity-0'
+      }`}
+      onClick={onClose}
+      onKeyDown={handleKeyDown}
+      aria-label="Close Menu"
+    >
+      <div className="h-screen w-1/2 bg-background-secondary transition-all duration-300 ease-in-out">
+        <div className="flex flex-col px-4 pt-5">
+          <button
+            className="mb-9 flex self-end"
+            onClick={onClose}
+            aria-label="Close Menu"
+          >
+            닫기
+          </button>
+          <div className="flex flex-col gap-6">
+            <HeaderBoardButton className="block md:hidden" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
