3f80f9295412cb54a454ea0762632ff0384a6bc0
홍지윤 <h_zhirun@naver.com>
2025-01-21 20:44:48 +0900

    feat: DatePicker 테스트 코드 구현

---
+++ src/app/(component)/datepicker/page.tsx
@@ -0,0 +1,9 @@
+import DatePicker from '@/app/(routes)/[teamid]/tasklist/components/DatePicker';
+
+export default function DatePickerExample() {
+  return (
+    <div className="ml-20 mt-20">
+      <DatePicker />
+    </div>
+  );
+}
