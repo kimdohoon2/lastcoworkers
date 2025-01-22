718197082e5887761541bc0d11a303d50448873d
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-22 13:18:39 +0900

    feat : 리엑트쿼리 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@/app/styles/globals.css';
 import Header from './components/common/header/Header';
 
@@ -8,6 +9,7 @@ export const metadata: Metadata = {
     '업무 배정과 현황 공유를 효율적으로 관리하며, 최적화된 To-do 리스트와 성과 지표 시각화로 사용자가 업무를 쉽게 생성하고 진행 상황을 확인할 수 있는 서비스입니다.',
 };
 
+const queryClient = new QueryClient();
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -23,8 +25,10 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <Header />
-        {children}
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
