f674deadca6edee481be8da927ff3f7031231910
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-23 21:58:14 +0900

    feat : 필요한 값들만 저장 omit을 이용하여 제외

--- src/app/stores/auth/authSlice.ts
+++ src/app/stores/auth/authSlice.ts
@@ -2,15 +2,18 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { SignInResponse } from '@/app/types/AuthType';
 
 interface AuthState {
-  user: SignInResponse['user'] | null;
-  accessToken: string | null;
-  refreshToken: string | null;
+  user: Omit<
+    SignInResponse['user'],
+    'updatedAt' | 'createdAt' | 'image'
+  > | null;
+  accessToken: string;
+  refreshToken: string;
 }
 
 const initialState: AuthState = {
   user: null,
-  accessToken: null,
-  refreshToken: null,
+  accessToken: '',
+  refreshToken: '',
 };
 
 const authSlice = createSlice({
@@ -19,14 +22,22 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action: PayloadAction<SignInResponse>) => {
       const { user, accessToken, refreshToken } = action.payload;
-      state.user = user;
-      state.accessToken = accessToken;
-      state.refreshToken = refreshToken;
+
+      // 필요한 필드만 저장 (Omit을 통해 필드 제외)
+      state.user = {
+        id: user.id,
+        email: user.email,
+        nickname: user.nickname,
+        teamId: user.teamId,
+      };
+
+      state.accessToken = accessToken ?? '';
+      state.refreshToken = refreshToken ?? '';
     },
     logout: (state) => {
       state.user = null;
-      state.accessToken = null;
-      state.refreshToken = null;
+      state.accessToken = '';
+      state.refreshToken = '';
     },
   },
 });
