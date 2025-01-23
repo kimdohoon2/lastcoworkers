f55f5fbd178ab0017b65517925fe002a37b79e2d
JunHee <z1top159@gmail.com>
2025-01-23 15:58:19 +0900

    feat: accessToken 관련 코드 주석처리

--- src/app/lib/instance.ts
+++ src/app/lib/instance.ts
@@ -10,13 +10,13 @@ const instance = axios.create({
 export default instance;
 
 // Access Token을 요청 헤더에 동적으로 추가
-instance.interceptors.request.use(
-  (config) => {
-    const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN; // 환경변수에서 토큰 가져오기
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
+// instance.interceptors.request.use(
+//   (config) => {
+//     const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN; // 환경변수에서 토큰 가져오기
+//     if (token) {
+//       config.headers['Authorization'] = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error),
+// );
