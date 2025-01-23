af95518d32e55feac0f44525c8a0843a36dde61d
홍지윤 <h_zhirun@naver.com>
2025-01-23 21:49:09 +0900

    feat: TaskCard 날짜와 반복 정보 컴포넌트화

--- src/app/components/tasklist/TaskCard.tsx
+++ src/app/components/tasklist/TaskCard.tsx
@@ -1,46 +1,29 @@
 import { Task } from '@/app/types/task';
-import TaskCardCalendar from '../icons/TaskCardCalendar';
-import IconRepeat from '../icons/IconRepeat';
+import clsx from 'clsx';
 import IconComment from '../icons/IconComment';
 import IconCheckBox from '../icons/IconCheckBox';
-import VerticalBar from '../icons/VerticalBar';
 import IconUncheckBox from '../icons/IconUncheckBox';
+import DateRepeatInfo from './DateRepeatInfo';
 
 export default function TaskCard({ task }: { task: Task }) {
-  const { name, commentCount, date, frequency, doneAt } = task;
-
-  const formatDate = (selectDate: string) => {
-    return new Date(selectDate).toLocaleDateString('ko-KR', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    });
-  };
+  const { name, commentCount, doneAt, date, frequency } = task;
 
   return (
     <div className="flex flex-col gap-2.5 rounded-lg bg-background-secondary px-3.5 py-3">
-      <div className="flex justify-between">
-        <div className="flex items-center gap-3">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
           {doneAt ? <IconCheckBox /> : <IconUncheckBox />}
-          <h3>{name}</h3>
-          <p className="text-default flex items-center gap-1 text-xs">
+          <h3 className={clsx('pr-1 text-md', { 'line-through': doneAt })}>
+            {name}
+          </h3>
+          <p className="flex items-center gap-1 text-xs text-text-default">
             <IconComment />
             {commentCount}
           </p>
         </div>
         {/* 수정/삭제 드롭다운 */}
       </div>
-      <div className="flex items-center gap-2.5">
-        <div className="text-default flex items-center gap-2">
-          <TaskCardCalendar />
-          {formatDate(date)}
-        </div>
-        <VerticalBar />
-        <div className="flex items-center gap-1.5">
-          <IconRepeat />
-          {frequency}
-        </div>
-      </div>
+      <DateRepeatInfo date={date} frequency={frequency} />
     </div>
   );
 }
