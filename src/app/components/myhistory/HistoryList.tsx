f3e1d72a19deb84f1ba5af97718636de731e2608
JunHee <z1top159@gmail.com>
2025-01-23 14:58:33 +0900

    style: 완료된 할 일 취소선 추가

--- src/app/components/myhistory/HistoryList.tsx
+++ src/app/components/myhistory/HistoryList.tsx
@@ -1,53 +1,36 @@
-'use client';
-
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
 import getHistory, { Task } from '@/app/lib/user/getHistory';
 import formatDate from '@/app/utils/formatDate';
 import IconCheckBox from '../icons/IconCheckBox';
 
-export default function HistoryList() {
-  // React Query를 사용해 데이터를 fetching
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['history'], // 쿼리 키 설정
-    queryFn: getHistory, // 데이터 패칭 함수
-  });
+// task.date 기준으로 데이터 그룹화
+const groupByDate = (tasks: Task[]) => {
+  return tasks.reduce<Record<string, Task[]>>(
+    (groups, task) => {
+      const { date } = task;
+      const sameDate = groups;
+      if (!groups[date]) {
+        sameDate[date] = [];
+      }
+      groups[date].push(task);
+      return groups;
+    },
+    {} as Record<string, Task[]>,
+  );
+};
 
-  const history: Task[] = data?.tasksDone || [];
+export default async function HistoryList() {
+  try {
+    // 서버에서 데이터를 가져옴
+    const data = await getHistory();
+    const history: Task[] = data.tasksDone || [];
+    const groupedHistory = groupByDate(history);
 
-  // task.date 기준으로 데이터 그룹화
-  const groupByDate = (tasks: Task[]) => {
-    return tasks.reduce<Record<string, Task[]>>(
-      (groups, task) => {
-        const { date } = task;
-        const sameDate = groups;
-        if (!groups[date]) {
-          sameDate[date] = [];
-        }
-        groups[date].push(task);
-        return groups;
-      },
-      {} as Record<string, Task[]>,
-    );
-  };
-
-  const groupedHistory = groupByDate(history);
-
-  if (isError) {
     return (
-      <p>
-        오류 발생: {error instanceof Error ? error.message : '알 수 없는 오류'}
-      </p>
-    );
-  }
-
-  return (
-    <div>
-      {isLoading ? (
-        <p>로딩 중...</p>
-      ) : (
-        <div>
-          {Object.entries(groupedHistory).map(([date, tasks]) => (
+      <div>
+        {Object.entries(groupedHistory).length === 0 ? (
+          <p className="text-md font-medium">아직 히스토리가 없습니다.</p>
+        ) : (
+          Object.entries(groupedHistory).map(([date, tasks]) => (
             <div key={date}>
               <h2 className="mb-4 text-lg font-medium">{formatDate(date)}</h2>
               <ul>
@@ -57,14 +40,22 @@ export default function HistoryList() {
                     key={task.id}
                   >
                     <IconCheckBox />
-                    <p className="ml-[0.4375rem] text-md">{task.name}</p>
+                    <p className="ml-[0.4375rem] text-md line-through">
+                      {task.name}
+                    </p>
                   </li>
                 ))}
               </ul>
             </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+          ))
+        )}
+      </div>
+    );
+  } catch (error) {
+    return (
+      <p>
+        오류 발생: {error instanceof Error ? error.message : '알 수 없는 오류'}
+      </p>
+    );
+  }
 }
