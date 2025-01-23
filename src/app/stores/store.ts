5e2cdfa7660567c65ed4339ff853c22a2b8c0970
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-23 21:57:33 +0900

    feat : 클라이언트사이드렌더링에서 사용하게 변경 불필요하는 초기값 제거

--- src/app/stores/store.ts
+++ src/app/stores/store.ts
@@ -1,46 +1,39 @@
+'use client';
+
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // 로컬 스토리지 사용
+import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
-import authReducer, { setCredentials } from '@/app/stores/auth/authSlice'; // setCredentials import
+import authReducer from '@/app/stores/auth/authSlice';
 
-// Redux Persist 설정
+// Persist 설정 간소화
 const persistConfig = {
-  key: 'root', // 스토리지에 저장될 키
+  key: 'root',
   storage,
+  // 특정 리듀서만 영구 저장
+  whitelist: ['auth'],
 };
 
-// 루트 리듀서 생성
+// 리듀서 결합
 const rootReducer = combineReducers({
-  auth: authReducer, // auth 상태를 관리
+  auth: authReducer,
 });
 
-// Persisted Reducer 생성
+// Persist 리듀서 생성
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// Redux 스토어 생성
+// 스토어 생성
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // redux-persist 관련 경고 방지
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
     }),
 });
 
-// Redux Persist 객체 생성
 const persistor = persistStore(store);
-
-// 로컬스토리지에서 초기값을 읽어 Redux에 설정
-const accessToken = localStorage.getItem('accessToken');
-const refreshToken = localStorage.getItem('refreshToken');
-const user = localStorage.getItem('user')
-  ? JSON.parse(localStorage.getItem('user')!)
-  : null;
-
-// 초기 상태 설정
-if (accessToken && refreshToken && user) {
-  store.dispatch(setCredentials({ accessToken, refreshToken, user }));
-}
 
 export { store, persistor };
 export type RootState = ReturnType<typeof store.getState>;
