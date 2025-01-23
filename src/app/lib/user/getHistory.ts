0a320fa498bc45a1501d5b6b2dcc38136966e494
JunHee <z1top159@gmail.com>
2025-01-23 13:44:01 +0900

    refactor: api 함수 오류 처리 간소화

--- src/app/lib/user/getHistory.ts
+++ src/app/lib/user/getHistory.ts
@@ -1,15 +1,5 @@
 import instance from '../instance';
 
-interface InstanceError {
-  message: string;
-  response?: {
-    status: number;
-    data?: {
-      message?: string;
-    };
-  };
-}
-
 type FrequencyType = 'DAILY' | 'WEEKLY' | 'MONTHLY' | 'ONCE';
 
 export interface Task {
@@ -32,16 +22,6 @@ export interface GetHistoryResponse {
 }
 
 export default async function getHistory(): Promise<GetHistoryResponse> {
-  try {
-    const { data } = await instance.get<GetHistoryResponse>(`/user/history`);
-    return data;
-  } catch (error: unknown) {
-    const err = error as InstanceError;
-    if (err.response) {
-      console.error('API 응답 에러:', err.response?.data);
-    }
-    throw new Error(
-      err.response?.data?.message || '완료한 작업을 불러오는데 실패했습니다.',
-    );
-  }
+  const { data } = await instance.get<GetHistoryResponse>(`/user/history`);
+  return data;
 }
