1ec3b0e9359e86c1de78da59510e59d17c5b406b
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-25 16:55:34 +0900

    feat : 401에러처리 추가
    - accesToken유효기간이 지나고 401에러가 뜨면서 관련로직이 잘 작동하는 콘솔 추가(추후에 제거 예정)

--- src/app/lib/instance.ts
+++ src/app/lib/instance.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { store } from '@/app/stores/store';
+import handleTokenRefresh from '@/app/lib/auth/refreshTokenHandler';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
@@ -10,14 +11,34 @@ const instance = axios.create({
 
 // 요청 인터셉터: Access Token 추가
 instance.interceptors.request.use((config) => {
+  console.log('요청 인터셉터 실행');
   const state = store.getState();
   const token = state.auth.accessToken;
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+    console.log('요청에 액세스 토큰 추가됨');
+  } else {
+    console.log('액세스 토큰 없음');
   }
 
   return config;
 });
+
+// 응답 인터셉터: 401 에러 처리
+instance.interceptors.response.use(
+  (response) => {
+    console.log('응답 성공');
+    return response;
+  },
+  async (error) => {
+    if (error.response?.status === 401) {
+      console.log('401 에러 발생: 토큰 리프레시 시도');
+      return handleTokenRefresh(error);
+    }
+    console.error('요청 실패:', error.message);
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
