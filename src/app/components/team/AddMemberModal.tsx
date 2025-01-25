58a024d9d72c58b1e0f9e48ec62e97ac4dae23cb
ksm <hpk5802@github.com>
2025-01-25 19:14:12 +0900

    feat: 멤버 초대 모달 추가

---
+++ src/app/components/team/AddMemberModal.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Button from '@/app/components/common/button/Button';
+import Modal from '@/app/components/common/modal/Modal';
+
+interface ModalProps {
+  token: string;
+  isOpen: boolean;
+  closeModal: () => void;
+}
+
+const SERVER_URL = 'localhost:3000/invitation';
+
+function AddMemberModal({ token, isOpen, closeModal }: ModalProps) {
+  const handleClick = () => {
+    navigator.clipboard.writeText(`${SERVER_URL}?token=${token}`);
+    closeModal();
+  };
+
+  if (token) return null;
+
+  return (
+    <Modal hasCloseBtn isOpen={isOpen} closeModal={closeModal}>
+      <div className="w-full px-12 text-center">
+        <div className="mb-2 text-lg font-medium">멤버 초대</div>
+        <div className="mb-6 text-md text-slate-400">
+          그룹에 참여할 수 있는 링크를 복사합니다.
+        </div>
+        <Button className="w-full text-text-inverse" onClick={handleClick}>
+          링크 복사하기
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default AddMemberModal;
