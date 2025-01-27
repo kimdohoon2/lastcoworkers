84cf0c96a123bad3bbcd2930772942cd999dfa7d
ksm <hpk5802@github.com>
2025-01-27 15:07:04 +0900

    feat: 드롭다운 추가

--- src/app/components/team/MemberCard.tsx
+++ src/app/components/team/MemberCard.tsx
@@ -6,6 +6,8 @@ import DropdownToggle from '@/app/components/common/dropdown/DropdownToggle';
 import DropdownList from '@/app/components/common/dropdown/DropdownList';
 import DropdownItem from '@/app/components/common/dropdown/DropdownItem';
 import useDropdown from '@/app/hooks/useDropdown';
+import useModal from '@/app/hooks/useModal';
+import DetailMemberModal from '@/app/components/team/DetailMemberModal';
 
 interface GroupMember {
   role: 'ADMIN' | 'MEMBER';
@@ -17,50 +19,50 @@ interface GroupMember {
 }
 
 function MemberCard({ member }: { member: GroupMember }) {
-  const { isOpen, toggleDropdown, closeDropdown, selectItem } = useDropdown();
+  const { isOpen, toggleDropdown, closeDropdown } = useDropdown();
+  const { isOpen: isModalOpen, openModal, closeModal } = useModal();
 
-  const handleItemClick = (item: string) => {
-    selectItem(item);
-  };
+  const handleExpel = () => {};
 
   return (
-    <div className="flex items-center justify-between gap-1.5 rounded-2xl bg-background-secondary px-4 py-3 tablet:px-6 tablet:py-5">
-      <div>
-        <div className="mb-1.5 flex items-center gap-2 tablet:relative tablet:mb-0.5 tablet:block tablet:pl-11">
-          <div className="relative h-6 w-6 tablet:absolute tablet:inset-0 tablet:h-8 tablet:w-8">
-            {member.userImage ? (
-              <Image src={member.userImage} fill alt="프로필 이미지" />
-            ) : (
-              <IconProfileEmpty className="h-full w-full" />
-            )}
+    <>
+      <div className="flex items-center justify-between gap-1.5 rounded-2xl bg-background-secondary px-4 py-3 tablet:px-6 tablet:py-5">
+        <div>
+          <div className="mb-1.5 flex items-center gap-2 tablet:relative tablet:mb-0.5 tablet:block tablet:pl-11">
+            <div className="relative h-6 w-6 tablet:absolute tablet:inset-0 tablet:h-8 tablet:w-8">
+              {member.userImage ? (
+                <Image src={member.userImage} fill alt="프로필 이미지" />
+              ) : (
+                <IconProfileEmpty className="h-full w-full" />
+              )}
+            </div>
+            <div className="text-md font-medium">{member.userName}</div>
           </div>
-          <div className="text-md font-medium">{member.userName}</div>
+          <div className="text-xs text-text-secondary tablet:ml-11">
+            {member.userEmail}
+          </div>
         </div>
-        <div className="text-xs text-text-secondary tablet:ml-11">
-          {member.userEmail}
-        </div>
-      </div>
-      <Dropdown onClose={closeDropdown}>
-        <DropdownToggle className="h-4 w-4" onClick={toggleDropdown}>
-          <TaskCardDropdown />
-        </DropdownToggle>
+        <Dropdown onClose={closeDropdown}>
+          <DropdownToggle className="h-4 w-4" onClick={toggleDropdown}>
+            <TaskCardDropdown />
+          </DropdownToggle>
 
-        <DropdownList className="right-0" isOpen={isOpen}>
-          <DropdownItem
-            onClick={() => handleItemClick('돈까스')}
-            onClose={closeDropdown}
-          >
-            멤버 정보
-          </DropdownItem>
-          <DropdownItem
-            onClick={() => handleItemClick('떡볶이')}
-            onClose={closeDropdown}
-          >
-            추방하기
-          </DropdownItem>
-        </DropdownList>
-      </Dropdown>
-    </div>
+          <DropdownList className="right-0" isOpen={isOpen}>
+            <DropdownItem onClick={openModal} onClose={closeDropdown}>
+              멤버 정보
+            </DropdownItem>
+            <DropdownItem onClick={handleExpel} onClose={closeDropdown}>
+              추방하기
+            </DropdownItem>
+          </DropdownList>
+        </Dropdown>
+      </div>
+      <DetailMemberModal
+        member={member}
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+      />
+    </>
   );
 }
 
