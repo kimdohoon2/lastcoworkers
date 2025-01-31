22f1764a633786923260b5c672b7f8424c47a211
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-31 17:07:00 +0900

    feat : postProviderApi 전반적으로 수정

--- src/app/lib/auth/postProviderApi.ts
+++ src/app/lib/auth/postProviderApi.ts
@@ -1,40 +1,20 @@
-import axios from 'axios';
 import instance from '@/app/lib/instance';
+import { SignInResponse } from '@/app/types/AuthType';
 
 interface OauthDataType {
-  state?: string;
-  redirectUri?: string;
+  state: string;
+  redirectUri: string;
   token: string;
-  provider: 'GOOGLE' | 'KAKAO';
 }
 
-const postProviderApi = async ({
-  state,
-  redirectUri,
-  token,
-  provider,
-}: OauthDataType) => {
-  const quickLoginData = { state, redirectUri, token };
+const postProviderApi = async (
+  provider: 'GOOGLE' | 'KAKAO',
+  data: OauthDataType,
+): Promise<SignInResponse> => {
+  console.log('Request data:', data);
 
-  console.log('Request data:', quickLoginData);
-  console.log('Provider:', provider);
-
-  try {
-    const response = await instance.post(
-      `/auth/signIn/${provider}`,
-      quickLoginData,
-    );
-    return response.data;
-  } catch (error: unknown) {
-    if (axios.isAxiosError(error) && error.response) {
-      console.error('Error response:', error.response.data);
-      console.error('Error status:', error.response.status);
-      console.error('Error headers:', error.response.headers);
-    } else {
-      console.error('Error:');
-    }
-    throw error; // 에러 재발생
-  }
+  const response = await instance.post(`/auth/signIn/${provider}`, data);
+  return response.data as SignInResponse;
 };
 
 export default postProviderApi;
