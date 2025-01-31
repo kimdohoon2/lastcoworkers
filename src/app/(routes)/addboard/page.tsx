3db379488075439e3c7d182154e6b4eeb69d7208
JunHee <z1top159@gmail.com>
2025-01-31 12:12:39 +0900

    feat: 게시글 쓰기 페이지 시맨틱태그

--- src/app/(routes)/addboard/page.tsx
+++ src/app/(routes)/addboard/page.tsx
@@ -1,3 +1,10 @@
 export default function addBoard() {
-  return <div>게시글 쓰기 페이지입니다.</div>;
+  return (
+    <div className="px-[16px] pt-[100px]">
+      <h1 className="border-primary border-b">게시글 쓰기</h1>
+      <h2>제목</h2>
+      <h3>내용</h3>
+      <h4>이미지</h4>
+    </div>
+  );
 }
