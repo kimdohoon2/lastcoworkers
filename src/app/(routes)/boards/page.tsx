4c2f7c0f1444430458d9974466fb28225fe1bfd8
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-01 22:32:55 +0900

    feat : 공통컴퍼넌트사용하여 page.tsx변경, gitkeep삭제

--- src/app/(routes)/boards/page.tsx
+++ src/app/(routes)/boards/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import BoardsSearchIcon from '@/app/components/icons/BoardsSearchIcon';
-import BoardsLikeIcon from '@/app/components/icons/BoardsLikeIcon';
 import Button from '@/app/components/common/button/Button';
 import IconPlus from '@/app/components/icons/IconPlus';
 import Dropdown from '@/app/components/common/dropdown/Dropdown';
@@ -13,6 +11,7 @@ import DropdownToggle from '@/app/components/common/dropdown/DropdownToggle';
 import IconToggle from '@/app/components/icons/IconToggle';
 import useDropdown from '@/app/hooks/useDropdown';
 import clsx from 'clsx';
+import CommonAriticleCard from '@/app/components/boards/CommonAriticleCard';
 
 export default function BoardsPage() {
   const { isOpen, toggleDropdown, closeDropdown, currentItem, selectItem } =
@@ -38,10 +37,10 @@ export default function BoardsPage() {
 
   return (
     <>
-      <section className="mt-16 px-4">
-        <div className="pt-8">
-          <div className="flex flex-col gap-6">
-            <h1 className="text-2lg">자유게시판</h1>
+      <section className="mt-16 px-4 tablet:px-6 xl:mx-auto xl:w-[75rem]">
+        <div className="pt-8 tablet:pt-10">
+          <div className="flex flex-col gap-6 tablet:gap-8 xl:gap-10">
+            <h1 className="text-2lg tablet:text-2xl">자유게시판</h1>
             {/* 검색창 */}
             <div className='className="flex w-full rounded-[12px] border border-[#F8FAFC1A] bg-background-secondary py-3'>
               <label className="w-full" htmlFor="search">
@@ -55,69 +54,33 @@ export default function BoardsPage() {
                 </div>
               </label>
             </div>
-            <h2>베스트 게시글</h2>
+            <h2 className="tablet:text-xl">베스트 게시글</h2>
             {/* 베스트 게시글 카드 */}
             {bestPosts.map((post) => (
-              <div
+              <CommonAriticleCard
                 key={post.id}
-                className="w-full rounded-[12px] border border-[#F8FAFC1A] bg-background-secondary px-4 pb-4 pt-2"
-              >
-                <div className="mb-3 flex items-center gap-2">
-                  <div className="w-full max-w-[14px]">
-                    <Image
-                      className="w-full"
-                      src="/icons/BoardsBestIcon.png"
-                      width={14}
-                      height={16}
-                      alt="베스트 아이콘"
-                    />
-                  </div>
-                  <span className="text-md">Best</span>
-                </div>
-                <div className="flex justify-between">
-                  <p className="text-md text-text-secondary">{post.title}</p>
-                  <div className="h-[64px] w-[64px]">
-                    <Image
-                      className="h-full w-full rounded-[12px] object-cover"
-                      src={post.image}
-                      alt="게시글 이미지"
-                      width={72}
-                      height={72}
-                    />
-                  </div>
-                </div>
-                <p className="mb-4 text-xs text-text-disabled">
-                  {new Date(post.updatedAt).toLocaleDateString()}
-                </p>
-                <div className="flex justify-between">
-                  <div className="flex items-center gap-3">
-                    <Image
-                      src="/icons/BoardsProfile.png"
-                      width={32}
-                      height={32}
-                      alt="프로필 이미지"
-                    />
-                    <span className="text-xs">{post.writer.nickname}</span>
-                  </div>
-                  <div className="flex items-center gap-1">
-                    <BoardsLikeIcon />
-                    <span className="text-xs text-text-disabled">
-                      {post.likeCount}
-                    </span>
-                  </div>
-                </div>
-              </div>
+                id={post.id}
+                title={post.title}
+                image={post.image}
+                updatedAt={post.updatedAt}
+                writer={post.writer}
+                likeCount={post.likeCount}
+                isBasic={false}
+              />
             ))}
           </div>
           <div>
-            <div className="my-8 h-[1px] w-full bg-[#F8FAFC1A]" />
+            <div className="my-8 h-[1px] w-full bg-[#F8FAFC1A] tablet:my-10" />
             <div className="mb-6 flex w-full items-center justify-between">
-              <h3>게시글</h3>
+              <h3 className="tablet:text-xl">게시글</h3>
               {/* 드랍다운공통컴퍼넌트사용하기 */}
 
-              <Dropdown className="w-24" onClose={closeDropdown}>
+              <Dropdown
+                className="w-24 tablet:w-[7.5rem]"
+                onClose={closeDropdown}
+              >
                 <DropdownToggle className="w-full" onClick={toggleDropdown}>
-                  <div className="flex w-full items-center justify-between rounded-xl bg-background-secondary px-2 py-[13.5px] text-center text-xs text-text-primary hover:bg-background-tertiary">
+                  <div className="flex w-full items-center justify-between rounded-xl bg-background-secondary px-2 py-[13.5px] text-xs text-text-primary hover:bg-background-tertiary tablet:px-4 tablet:text-md">
                     {currentItem || '최신순'}
                     <IconToggle
                       className={clsx('transition-transform', {
@@ -130,14 +93,14 @@ export default function BoardsPage() {
 
                 <DropdownList className="mt-[6px] w-full" isOpen={isOpen}>
                   <DropdownItem
-                    className="pl-2 text-start text-xs"
+                    className="pl-2 text-start text-xs tablet:pl-4 tablet:text-md"
                     onClick={() => handleItemClick('최신순')}
                     onClose={closeDropdown}
                   >
                     최신순
                   </DropdownItem>
                   <DropdownItem
-                    className="pl-2 text-start text-xs"
+                    className="pl-2 text-start text-xs tablet:pl-4 tablet:text-md"
                     onClick={() => handleItemClick('좋아요 많은순')}
                     onClose={closeDropdown}
                   >
@@ -149,62 +112,24 @@ export default function BoardsPage() {
             {/* 게시글 카드 */}
             <div className="flex flex-col gap-4">
               {bestPosts.map((post) => (
-                <div
+                <CommonAriticleCard
                   key={post.id}
-                  className="w-full rounded-[12px] border border-[#F8FAFC1A] bg-background-secondary px-4 pb-4 pt-2"
-                >
-                  <div className="mb-3 flex items-center gap-2">
-                    <div className="w-full max-w-[14px]">
-                      <Image
-                        className="w-full"
-                        src="/icons/BoardsBestIcon.png"
-                        width={14}
-                        height={16}
-                        alt="베스트 아이콘"
-                      />
-                    </div>
-                    <span className="text-md">Best</span>
-                  </div>
-                  <div className="flex justify-between">
-                    <p className="text-md text-text-secondary">{post.title}</p>
-                    <div className="h-[64px] w-[64px]">
-                      <Image
-                        className="h-full w-full rounded-[12px] object-cover"
-                        src={post.image}
-                        alt="게시글 이미지"
-                        width={72}
-                        height={72}
-                      />
-                    </div>
-                  </div>
-                  <p className="mb-4 text-xs text-text-disabled">
-                    {new Date(post.updatedAt).toLocaleDateString()}
-                  </p>
-                  <div className="flex justify-between">
-                    <div className="flex items-center gap-3">
-                      <Image
-                        src="/icons/BoardsProfile.png"
-                        width={32}
-                        height={32}
-                        alt="프로필 이미지"
-                      />
-                      <span className="text-xs">{post.writer.nickname}</span>
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <BoardsLikeIcon />
-                      <span className="text-xs text-text-disabled">
-                        {post.likeCount}
-                      </span>
-                    </div>
-                  </div>
-                </div>
+                  id={post.id}
+                  title={post.title}
+                  image={post.image}
+                  updatedAt={post.updatedAt}
+                  writer={post.writer}
+                  likeCount={post.likeCount}
+                  isBest={false}
+                  tabletHidden
+                />
               ))}
             </div>
           </div>
         </div>
       </section>
       <Link
-        className="fixed bottom-5 right-4 block h-[48px] w-[125px]"
+        className="fixed bottom-5 right-4 block h-[48px] w-[125px] tablet:right-8 xl:bottom-9 xl:right-96"
         href="/addboard"
       >
         <Button variant="plus" size="plus">
