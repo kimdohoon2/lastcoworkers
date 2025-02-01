04fb78ecf7b9faadf38778d25384ecd5ea188abf
ksm <hpk5802@github.com>
2025-02-01 20:09:22 +0900

    feat: 로그인 안 된 경우 login 페이지로 리다이렉트

--- src/app/(routes)/addteam/page.tsx
+++ src/app/(routes)/addteam/page.tsx
@@ -6,9 +6,13 @@ import postGroup from '@/app/lib/group/postGroup';
 import { GroupData } from '@/app/types/group';
 import TeamForm from '@/app/components/team/TeamForm';
 import { useRouter } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/app/stores/store';
+import { useEffect } from 'react';
 
 function Page() {
   const router = useRouter();
+  const { accessToken } = useSelector((state: RootState) => state.auth);
 
   const onSubmit = async ({ profile, name }: FieldValues) => {
     let imageUrl: string | null = null;
@@ -50,6 +54,13 @@ function Page() {
       alert('팀 생성에 실패했습니다.');
     }
   };
+
+  useEffect(() => {
+    if (!accessToken) {
+      alert('로그인 후 이용할 수 있습니다.');
+      router.push('/login');
+    }
+  }, [accessToken, router]);
 
   return (
     <div>
