e6f433eea99a972fd806f4ccf090ebac31fb6160
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-02 17:32:40 +0900

    feat : 자유게시판 클릭시 사이드바 없어지면서 해당페이지로 이동 로직추가

--- src/app/components/common/header/Boards.tsx
+++ src/app/components/common/header/Boards.tsx
@@ -2,15 +2,18 @@ import Link from 'next/link';
 
 interface HeaderBoardButtonProps {
   className?: string;
+  onClick?: () => void;
 }
 
 export default function HeaderBoardButton({
   className,
+  onClick,
 }: HeaderBoardButtonProps) {
   return (
     <Link
       className={`${className || ''} hover:text-interaction-hover`}
       href="/boards"
+      onClick={onClick}
       aria-label="자유게시판으로 이동"
     >
       자유게시판
