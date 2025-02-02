2d09597a54b3690d0999561015f034e922eb3523
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-02 17:38:23 +0900

    feat : 자유게시판 데이터 가져오는  api 로직 완성
    - 자주사용하는 타입 ArticleType으로 묶어서 사용
    - 리엑트쿼리 사용하여 getAritcle에  params를 가져와서 쉽게 사용

---
+++ src/app/lib/article/getArticle.ts
@@ -0,0 +1,12 @@
+import instance from '@/app/lib/instance';
+import { GetArticleRequest, GetArticleResponse } from '@/app/types/ArticleType';
+
+export default async function getArticle(
+  data: GetArticleRequest,
+): Promise<GetArticleResponse> {
+  const response = await instance.get<GetArticleResponse>('/articles', {
+    params: data,
+  });
+  console.log(response.data);
+  return response.data;
+}
