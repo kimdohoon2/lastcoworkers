d283f33e904473201255684f5a4c04e762413507
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-05 15:03:52 +0900

    feat : react-intersection-observer 라이브러리 사용해서 무한스크롤 구현완료

--- src/app/(routes)/boards/page.tsx
+++ src/app/(routes)/boards/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import Link from 'next/link';
 import Button from '@/app/components/common/button/Button';
 import IconPlus from '@/app/components/icons/IconPlus';
@@ -9,6 +10,7 @@ import useGetArticle from '@/app/hooks/useGetArticle';
 import useDebounce from '@/app/hooks/useDebounce';
 import BoardsOrderDropDown from '@/app/components/boards/BoardsOrderDropDown';
 import BoardsSearchBar from '@/app/components/boards/BoardsSearchBar';
+import useGetArticleInfinite from '@/app/hooks/useGetArticleInfinite';
 
 export default function BoardsPage() {
   const [windowWidth, setWindowWidth] = useState(
@@ -41,12 +43,28 @@ export default function BoardsPage() {
     keyword: debouncedSearchKeyword,
   });
 
-  const { data: recentPosts, isLoading: isRecentLoading } = useGetArticle({
-    page: 1,
-    pageSize: 10,
+  const {
+    data: recentPosts,
+    isLoading: isRecentLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useGetArticleInfinite({
+    pageSize: 3,
     orderBy: sortOrder,
     keyword: debouncedSearchKeyword,
   });
+
+  const allArticles = recentPosts?.pages.flatMap((page) => page.list) || [];
+
+  // 리엑트 인터셉션 옵저버 활용한 무한스크롤
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage, isFetchingNextPage]);
 
   return (
     <>
@@ -93,34 +111,52 @@ export default function BoardsPage() {
               <BoardsOrderDropDown setSortOrder={setSortOrder} />
             </div>
             {/* 게시글 카드 */}
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col">
               {isRecentLoading ? (
                 <div>게시글 가져오는 중</div>
-              ) : recentPosts && recentPosts.list.length > 0 ? (
-                <div className="flex flex-col gap-4 tablet:gap-6">
-                  {recentPosts?.list.map((article) => (
-                    <CommonAriticleCard
-                      key={article.id}
-                      id={article.id}
-                      title={article.title}
-                      image={article.image}
-                      updatedAt={article.updatedAt}
-                      writer={article.writer}
-                      likeCount={article.likeCount}
-                      isBest={false}
-                      isOnlyTablet={false}
-                      tabletHidden
-                      isLiked
-                    />
+              ) : allArticles.length > 0 ? (
+                <div className="flex flex-col">
+                  {allArticles.map((article) => (
+                    <div key={article.id} className="mb-4 tablet:mb-6">
+                      <CommonAriticleCard
+                        id={article.id}
+                        title={article.title}
+                        image={article.image}
+                        updatedAt={article.updatedAt}
+                        writer={article.writer}
+                        likeCount={article.likeCount}
+                        isBest={false}
+                        isOnlyTablet={false}
+                        tabletHidden
+                        isLiked
+                      />
+                    </div>
                   ))}
                 </div>
               ) : (
                 <div>검색 결과에 해당하는 게시글이 없습니다.</div>
               )}
+
+              {/* 모든 게시글을 가져왔을 때 메시지 표시 */}
+              {!hasNextPage && (
+                <div className="py-4 text-center">
+                  더 이상 게시글이 없습니다.
+                </div>
+              )}
+
+              {/* 무한 스크롤 시 UI 추가 */}
+              {isFetchingNextPage && (
+                <div className="py-4 text-center">게시글 가져오는 중</div>
+              )}
+
+              <div ref={ref} className="h-10">
+                {hasNextPage && !isFetchingNextPage && '스크롤하여 더 보기'}
+              </div>
             </div>
           </div>
         </div>
       </section>
+
       <Link
         className="fixed bottom-5 right-4 block h-[48px] w-[125px] tablet:right-8 xl:bottom-9 xl:right-96"
         href="/addboard"
