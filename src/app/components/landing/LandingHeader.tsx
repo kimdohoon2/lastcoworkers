50d3fbdf4b00d7934da356878b78d72658f23624
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-02-05 20:38:08 +0900

    feat:로그인 상태에 따른 페이지 이동 설정

--- src/app/components/landing/LandingHeader.tsx
+++ src/app/components/landing/LandingHeader.tsx
@@ -1,10 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import IconRepair from '@/app/components/icons/IconRepair';
-import Link from 'next/link';
+import getUser from '@/app/lib/user/getUser';
 
 export default function LandingHeader() {
+  const router = useRouter();
+
+  const handleStartClick = async () => {
+    try {
+      const userData = await getUser();
+      if (userData.memberships.length === 0) {
+        router.push('/noteam');
+      } else {
+        const latestGroup =
+          userData.memberships[userData.memberships.length - 1].group;
+        router.push(`/${latestGroup.id}`);
+      }
+    } catch (error) {
+      router.push('/login');
+    }
+  };
+
   return (
     <div className="mt-14 flex h-[40rem] w-full flex-col items-center justify-between pb-8 tablet:h-[58.75rem] tablet:pb-[10.5rem] xl:h-[67.5rem] xl:pb-[11.25rem]">
-      <div className="h-full w-full bg-landing-top-small bg-cover bg-center bg-no-repeat tablet:bg-landing-top-medium xl:bg-landing-top-large">
+      <div className="bg-landing-top-small tablet:bg-landing-top-medium xl:bg-landing-top-large h-full w-full bg-cover bg-center bg-no-repeat">
         <div className="flex flex-col items-center gap-1 tablet:gap-2 xl:gap-5">
           <div className="mt-[5.25rem] flex items-center gap-1 tablet:gap-4 xl:gap-6">
             <span className="text-2xl font-semibold text-text-primary tablet:text-4xl xl:text-5xl">
@@ -17,12 +37,12 @@ export default function LandingHeader() {
           </div>
         </div>
       </div>
-      <Link
-        href="/login"
+      <button
+        onClick={handleStartClick}
         className="cursor-pointer rounded-[2rem] bg-gradient-to-r from-brand-primary to-brand-tertiary px-[8.9375rem] py-3 text-base font-semibold text-white"
       >
         지금 시작하기
-      </Link>
+      </button>
     </div>
   );
 }
