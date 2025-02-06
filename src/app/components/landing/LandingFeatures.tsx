28c5915f9493715995bf8a8c19b04353aad118c3
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-02-06 15:52:10 +0900

    feat: 메인 페이지 features section에 따른 애니메이션 추가

--- src/app/components/landing/LandingFeatures.tsx
+++ src/app/components/landing/LandingFeatures.tsx
@@ -1,12 +1,32 @@
+'use client';
+
 import Image from 'next/image';
 import IconMessage from '@/app/components/icons/IconMessage';
 import IconFolder from '@/app/components/icons/IconFolder';
 import IconDone from '@/app/components/icons/IconDone';
+import { motion } from 'framer-motion';
 
 export default function LandingFeatures() {
+  const leftVariant = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const rightVariant = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
     <div className="flex flex-col gap-6 xl:gap-20">
-      <div className="mx-auto h-[29.1875rem] w-[21.4375rem] flex-col rounded-[2.5rem] bg-gradient-to-r from-brand-primary to-[#CEF57E] p-[0.0625rem] shadow-custom tablet:h-[22.125rem] tablet:w-[43.5rem] xl:h-[26.1875rem] xl:w-[62.25rem]">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.7 }}
+        variants={leftVariant}
+        className="shadow-custom mx-auto h-[29.1875rem] w-[21.4375rem] flex-col rounded-[2.5rem] bg-gradient-to-r from-brand-primary to-[#CEF57E] p-[0.0625rem] tablet:h-[22.125rem] tablet:w-[43.5rem] xl:h-[26.1875rem] xl:w-[62.25rem]"
+      >
         <div className="flex h-full w-full flex-wrap-reverse items-start rounded-[2.5rem] bg-background-primary pl-[3.125rem] tablet:justify-center tablet:gap-[8.75rem] tablet:pl-0 xl:gap-[12.0625rem]">
           <Image
             src="/contents/landing-section1.png"
@@ -24,8 +44,15 @@ export default function LandingFeatures() {
             </p>
           </div>
         </div>
-      </div>
-      <div className="mx-auto flex h-[29.1875rem] w-[21.4375rem] flex-col gap-10 rounded-[2.5rem] border border-[#F8FAFC1a] bg-background-secondary pl-[3.125rem] tablet:h-[22.125rem] tablet:w-[43.5rem] tablet:flex-row-reverse tablet:items-start tablet:justify-center tablet:gap-[8.75rem] tablet:pl-0 xl:h-[26.1875rem] xl:w-[62.25rem] xl:gap-[12.0625rem]">
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.7 }}
+        variants={rightVariant}
+        className="mx-auto flex h-[29.1875rem] w-[21.4375rem] flex-col gap-10 rounded-[2.5rem] border border-[#F8FAFC1a] bg-background-secondary pl-[3.125rem] tablet:h-[22.125rem] tablet:w-[43.5rem] tablet:flex-row-reverse tablet:items-start tablet:justify-center tablet:gap-[8.75rem] tablet:pl-0 xl:h-[26.1875rem] xl:w-[62.25rem] xl:gap-[12.0625rem]"
+      >
         <Image
           src="/contents/landing-section2.png"
           alt="랜딩 페이지 이미지2"
@@ -42,8 +69,15 @@ export default function LandingFeatures() {
             초대하세요
           </p>
         </div>
-      </div>
-      <div className="mx-auto flex h-[29.1875rem] w-[21.4375rem] flex-col gap-10 rounded-[2.5rem] bg-state-950 pl-[3.125rem] tablet:h-[22.125rem] tablet:w-[43.5rem] tablet:flex-row tablet:items-start tablet:justify-center tablet:gap-[8.75rem] xl:h-[26.1875rem] xl:w-[62.25rem] xl:gap-[12.0625rem]">
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.7 }}
+        variants={leftVariant}
+        className="bg-state-950 mx-auto flex h-[29.1875rem] w-[21.4375rem] flex-col gap-10 rounded-[2.5rem] pl-[3.125rem] tablet:h-[22.125rem] tablet:w-[43.5rem] tablet:flex-row tablet:items-start tablet:justify-center tablet:gap-[8.75rem] xl:h-[26.1875rem] xl:w-[62.25rem] xl:gap-[12.0625rem]"
+      >
         <Image
           src="/contents/landing-section3.png"
           alt="랜딩 페이지 이미지3"
@@ -61,7 +95,7 @@ export default function LandingFeatures() {
             체크해요
           </p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
