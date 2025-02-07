01f5791121c59707e740dad237d9b2fe9d861da7
ksm <hpk5802@github.com>
2025-02-07 13:53:11 +0900

    design: UX 개선 - radius, margin 수정

--- src/app/components/team/MemberCard.tsx
+++ src/app/components/team/MemberCard.tsx
@@ -50,7 +50,7 @@ function MemberCard({ member }: { member: GroupMember }) {
       <div className="flex items-center justify-between gap-1.5 rounded-2xl bg-background-secondary px-4 py-3 tablet:px-6 tablet:py-5">
         <div>
           <div className="mb-1.5 flex items-center gap-2 tablet:relative tablet:mb-0.5 tablet:block tablet:pl-11">
-            <div className="relative h-6 w-6 tablet:absolute tablet:inset-0 tablet:h-8 tablet:w-8">
+            <div className="relative h-6 w-6 overflow-hidden rounded-full tablet:absolute tablet:inset-0 tablet:h-8 tablet:w-8">
               {member.userImage ? (
                 <Image src={member.userImage} fill alt="프로필 이미지" />
               ) : (
