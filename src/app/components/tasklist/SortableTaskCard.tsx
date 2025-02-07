1702b361575ab8b0abae6234834e6256f7e8ebf5
홍지윤 <h_zhirun@naver.com>
2025-02-07 10:48:19 +0900

    style: 드래그 앤 드랍 스타일 지정(zindex, opacity)

--- src/app/components/tasklist/SortableTaskCard.tsx
+++ src/app/components/tasklist/SortableTaskCard.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useSortable } from '@dnd-kit/sortable'; // 개별 요소 드래그 훅
-import { CSS } from '@dnd-kit/utilities';
 import TaskCard from '@/app/components/tasklist/TaskCard';
 import { Task } from '@/app/types/task';
+import clsx from 'clsx';
 
 interface SortableTaskCardProps {
   task: Task;
@@ -16,18 +16,28 @@ export default function SortableTaskCard({
   groupId,
   taskListId,
 }: SortableTaskCardProps) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({
-      id: task.id,
-      transition: {
-        duration: 150,
-        easing: 'ease-in-out',
-      },
-    });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: task.id,
+    transition: {
+      duration: 150,
+      easing: 'ease-in-out',
+    },
+  });
 
   const style = {
-    transform: CSS.Transform.toString(transform),
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : 'none',
     transition,
+    zIndex: isDragging ? 10 : 'auto',
+    opacity: isDragging ? 0.8 : 1,
   };
 
   return (
@@ -36,7 +46,9 @@ export default function SortableTaskCard({
       style={style}
       {...attributes}
       {...listeners} // 드래그 이벤트 감지
-      className="relative cursor-grab active:cursor-grabbing"
+      className={clsx('relative cursor-grab active:cursor-grabbing', {
+        'pointer-events-none': isDragging,
+      })}
     >
       <TaskCard teamId={groupId} taskListId={taskListId} taskId={task.id} />
     </div>
