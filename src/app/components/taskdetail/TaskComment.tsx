6b5587c3e47d7699d82561e924fe45aa8f3b6b17
홍지윤 <h_zhirun@naver.com>
2025-02-11 17:30:58 +0900

    feat: 할 일 댓글 로딩 중일 때 스켈레톤 UI 렌더링 하도록 구현

--- src/app/components/taskdetail/TaskComment.tsx
+++ src/app/components/taskdetail/TaskComment.tsx
@@ -2,6 +2,7 @@ import { useTaskCommentQuery } from '@/app/lib/comment/getComment';
 import { Dispatch, SetStateAction } from 'react';
 import TaskCommentInput from './TaskCommentInput';
 import TaskCommentCard from './TaskCommentCard';
+import TaskCommentSkeleton from './TaskCommentSkeleton';
 
 function TaskComments({
   taskId,
@@ -13,17 +14,27 @@ function TaskComments({
   const { data, isLoading, error } = useTaskCommentQuery(taskId);
   const comments = data ?? [];
 
-  if (isLoading) return <p>댓글을 불러오는 중..</p>;
-  if (error) return <p>댓글을 불러오는 중 오류가 발생했습니다.</p>;
-
   return (
     <div className="mb-24 flex flex-col gap-6">
       <TaskCommentInput taskId={taskId} />
-      {comments?.length === 0 ? (
+
+      {error && (
+        <p className="text-text-default">
+          댓글을 불러오는 중 오류가 발생했습니다.
+        </p>
+      )}
+
+      {isLoading ? (
+        <div className="flex flex-col gap-4">
+          {Array.from({ length: 3 }, (_, i) => (
+            <TaskCommentSkeleton key={`skeleton-${i}`} />
+          ))}
+        </div>
+      ) : comments.length === 0 ? (
         <p className="text-text-default">댓글이 없습니다.</p>
       ) : (
         <ul>
-          {comments?.map((comment) => (
+          {comments.map((comment) => (
             <TaskCommentCard
               key={comment.id}
               taskId={taskId}
