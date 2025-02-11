174d36a74f0182064c90702456ee8115bf969a47
홍지윤 <h_zhirun@naver.com>
2025-02-11 15:53:57 +0900

    feat: 할 일 카드 스켈레톤 UI 구현

---
+++ src/app/components/tasklist/TaskCardSkeleton.tsx
@@ -0,0 +1,32 @@
+interface TaskCardSkeletonProps {
+  className?: string;
+}
+
+export default function TaskCardSkeleton({
+  className = '',
+}: TaskCardSkeletonProps) {
+  return (
+    <div
+      className={`flex w-full animate-pulse flex-col gap-2.5 rounded-[12px] rounded-lg border border-[#F8FAFC1A] bg-background-secondary px-3.5 py-3 shinny ${className}`}
+    >
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-nowrap items-center justify-between gap-2">
+          <div className="flex flex-grow items-center gap-2">
+            <span className="m-1 h-5 w-5 rounded bg-background-tertiary" />
+
+            <span className="h-4 w-1/3 rounded bg-background-tertiary" />
+
+            <p className="ml-auto flex items-center gap-1 tablet:ml-2">
+              <span className="h-4 w-8 rounded bg-background-tertiary" />
+            </p>
+          </div>
+
+          <span className="h-5 w-5 rounded bg-background-tertiary" />
+        </div>
+        <div className="flex gap-2">
+          <div className="h-4 w-3/5 rounded bg-background-tertiary tablet:w-1/3" />
+        </div>
+      </div>
+    </div>
+  );
+}
