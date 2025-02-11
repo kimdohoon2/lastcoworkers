68200fa72ff9882b88183933fdd590885891a74d
홍지윤 <h_zhirun@naver.com>
2025-02-11 19:01:14 +0900

    feat: 할 일 상세 스켈레톤 UI 구현

---
+++ src/app/components/taskdetail/TaskDetailSkeleton.tsx
@@ -0,0 +1,29 @@
+function TaskDetailSkeleton() {
+  return (
+    <div className="shiny animate-pulse bg-background-secondary p-4 tablet:p-6 xl:p-10">
+      <div className="flex flex-col gap-3">
+        <div className="h-6 w-6 rounded bg-background-tertiary" />
+
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-between">
+            <div className="h-6 w-3/4 rounded bg-background-tertiary" />
+            <div className="h-6 w-6 rounded bg-background-tertiary" />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-3">
+              <div className="h-8 w-8 rounded-full bg-background-tertiary" />
+              <div className="h-4 w-20 rounded bg-background-tertiary" />
+            </div>
+            <div className="h-4 w-20 rounded bg-background-tertiary" />
+          </div>
+
+          <div className="h-5 w-[18.5rem] rounded bg-background-tertiary" />
+          <div className="mt-3 h-[6rem] rounded-xl bg-background-tertiary tablet:h-[8rem] xl:h-[10rem]" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskDetailSkeleton;
