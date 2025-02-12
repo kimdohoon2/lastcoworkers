f15679375b008276d0fc72184057664b7d849eb8
ksm <hpk5802@github.com>
2025-02-12 13:51:00 +0900

    design: 스켈레톤 공통 애니메이션 적용

--- src/app/components/team/MemberCardSkeleton.tsx
+++ src/app/components/team/MemberCardSkeleton.tsx
@@ -5,10 +5,10 @@ function MemberCardSkeleton() {
     <div className="flex w-full items-center justify-between gap-1.5 rounded-2xl bg-background-secondary px-4 py-3 tablet:px-6 tablet:py-5">
       <div className="w-full overflow-hidden">
         <div className="mb-1.5 flex items-center gap-2 tablet:relative tablet:mb-0.5 tablet:block tablet:pl-11">
-          <div className="h-6 w-6 flex-shrink-0 rounded-full bg-background-tertiary tablet:absolute tablet:inset-0 tablet:h-8 tablet:w-8" />
-          <div className="h-4 w-full rounded-sm bg-background-tertiary" />
+          <div className="h-6 w-6 flex-shrink-0 rounded-full bg-background-tertiary shinny tablet:absolute tablet:inset-0 tablet:h-8 tablet:w-8" />
+          <div className="h-4 w-full rounded-sm bg-background-tertiary shinny" />
         </div>
-        <div className="h-3.5 w-full rounded-sm bg-background-tertiary tablet:ml-11" />
+        <div className="h-3.5 w-full rounded-sm bg-background-tertiary shinny tablet:ml-11" />
       </div>
       <TaskCardDropdown />
     </div>
