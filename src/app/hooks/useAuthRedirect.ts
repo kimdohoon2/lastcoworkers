8de9cbe02e07ad18a7c9064dacca2596d8c7dc19
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-12 19:43:08 +0900

    Refactor : setTimeout 삭제 router삭제

--- src/app/hooks/useAuthRedirect.ts
+++ src/app/hooks/useAuthRedirect.ts
@@ -1,23 +1,18 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@/app/stores/store';
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
 
 const useAuthRedirect = () => {
-  const router = useRouter();
   const { accessToken } = useSelector((state: RootState) => state.auth);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!accessToken) {
-      setTimeout(() => {
-        alert('로그인 후 이용할 수 있습니다.');
-        router.replace('/login');
-      }, 300);
+      alert('로그인 후 이용할 수 있습니다.');
     } else {
       setIsLoading(false);
     }
-  }, [accessToken, router]);
+  }, [accessToken]);
 
   return { isLoading };
 };
