9e1ca98e25b7797dcdd0cf9588501b6d08305c9a
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-13 11:44:44 +0900

    Refactor : 그전에 했던 코드로 롤백완료

--- src/app/hooks/useAuthRedirect.ts
+++ src/app/hooks/useAuthRedirect.ts
@@ -1,18 +1,23 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@/app/stores/store';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const useAuthRedirect = () => {
+  const router = useRouter();
   const { accessToken } = useSelector((state: RootState) => state.auth);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!accessToken) {
-      alert('로그인 후 이용할 수 있습니다.');
+      setTimeout(() => {
+        alert('로그인 후 이용할 수 있습니다.');
+        router.replace('/login');
+      }, 300);
     } else {
       setIsLoading(false);
     }
-  }, [accessToken]);
+  }, [accessToken, router]);
 
   return { isLoading };
 };
