1d8d23661d66530753dfb87e8b9c914c8d147e0e
홍지윤 <h_zhirun@naver.com>
2025-02-13 17:10:20 +0900

    refactor: 할 일 리스트/상세 모달 리팩토링 - 절대 경로로 수정 - ConfirmModal props 수정 및 추가 - ConfirmModal 재사용하여 코드 간소화 - 불필요한 태그 및 스타일 삭제

--- src/app/components/taskdetail/DeleteCommentModal.tsx
+++ src/app/components/taskdetail/DeleteCommentModal.tsx
@@ -1,38 +1,34 @@
 'use client';
 
-import Modal from '../common/modal/Modal';
-import Button from '../common/button/Button';
-import IconAlert from '../icons/IconAlert';
+import ConfirmModal from '@/app/components/common/modal/ConfirmModal';
+
+interface DeleteCommentModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onDelete: () => void;
+  isConfirmDisabled?: boolean;
+}
 
 export default function DeleteCommentModal({
   isOpen,
   onClose,
   onDelete,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-  onDelete: () => void;
-}) {
+  isConfirmDisabled = false,
+}: DeleteCommentModalProps) {
   return (
-    <Modal isOpen={isOpen} closeModal={onClose}>
-      <div className="flex flex-col items-center">
-        <IconAlert />
-        <div className="mt-4 flex w-[239px] flex-col items-center">
-          <h2 className="mb-4 text-lg font-light">댓글을 삭제하시겠어요?</h2>
-          <p className="mb-6 text-center text-md font-thin">
-            삭제 후에는 되돌릴 수 없습니다.
-          </p>
+    <ConfirmModal
+      isModalOpen={isOpen}
+      title={
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h2 className="text-lg font-light">댓글을 삭제하시겠어요?</h2>
+          <p className="text-md font-thin">삭제 후에는 되돌릴 수 없습니다.</p>
         </div>
-
-        <div className="flex justify-end gap-4">
-          <Button onClick={onClose} variant="secondary" className="w-[8.5rem]">
-            닫기
-          </Button>
-          <Button onClick={onDelete} variant="danger" className="w-[8.5rem]">
-            삭제하기
-          </Button>
-        </div>
-      </div>
-    </Modal>
+      }
+      cancelLabel="닫기"
+      confirmLabel="삭제하기"
+      handleCancel={onClose}
+      handleConfirm={onDelete}
+      isConfirmDisabled={isConfirmDisabled}
+    />
   );
 }
--- src/app/components/tasklist/DeleteRecurringModal.tsx
+++ src/app/components/tasklist/DeleteRecurringModal.tsx
@@ -1,9 +1,7 @@
-import { useDeleteRecurringMutation } from '@/app/lib/task/deleteTask';
-import { useQueryClient } from '@tanstack/react-query';
 import { useAppSelector } from '@/app/stores/hooks';
-import Button from '../common/button/Button';
-import Modal from '../common/modal/Modal';
-import IconAlert from '../icons/IconAlert';
+import { useQueryClient } from '@tanstack/react-query';
+import { useDeleteRecurringMutation } from '@/app/lib/task/deleteTask';
+import ConfirmModal from '@/app/components/common/modal/ConfirmModal';
 
 interface DeleteRecurringModalProps {
   isOpen: boolean;
@@ -50,54 +48,27 @@ export default function DeleteRecurringModal({
   };
 
   return (
-    <div
-      role="button"
-      tabIndex={0}
-      onClick={(e) => e.stopPropagation()}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          e.preventDefault();
-        }
-      }}
-      className={`${isOpen ? 'block' : 'hidden'}`}
-    >
-      <Modal isOpen={isOpen} closeModal={onClose}>
+    <ConfirmModal
+      isModalOpen={isOpen}
+      title={
         <div className="flex flex-col items-center gap-2 text-center">
-          <IconAlert />
-          <p className="mt-2 text-xl text-text-primary">{`'${task.name}'`}</p>
+          <p className="text-xl text-text-primary">{`'${task.name}'`}</p>
           <p className="text-lg text-text-primary">
             할 일의 반복을 정말 삭제하시겠어요?
           </p>
           <p className="text-md text-text-secondary">
             삭제 후에는 되돌릴 수 없습니다.
           </p>
-
-          <div className="mt-5 flex gap-2">
-            <Button
-              type="button"
-              className="w-[8.5rem] text-text-default"
-              variant="secondary"
-              size="large"
-              onClick={(e) => {
-                e.stopPropagation();
-                onClose();
-              }}
-            >
-              닫기
-            </Button>
-            <Button
-              type="button"
-              className="w-[8.5rem]"
-              variant="danger"
-              size="large"
-              onClick={handleDelete}
-              disabled={isPending}
-            >
-              삭제하기
-            </Button>
-          </div>
         </div>
-      </Modal>
-    </div>
+      }
+      cancelLabel="닫기"
+      confirmLabel="삭제하기"
+      handleCancel={(e) => {
+        if (e) e.stopPropagation();
+        onClose();
+      }}
+      handleConfirm={handleDelete}
+      isConfirmDisabled={isPending}
+    />
   );
 }
