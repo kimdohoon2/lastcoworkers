a04681891fd0e5e45c5363ec1711c9d25f25c432
홍지윤 <h_zhirun@naver.com>
2025-02-14 11:59:29 +0900

    refactor: 할 일 목록 생성 모달 리팩토링 - 절대 경로로 수정 - 불필요한 div 태그 제거 - useForm에서 handlesubmit 구조분해 할당 추가

--- src/app/components/tasklist/CreateListModal.tsx
+++ src/app/components/tasklist/CreateListModal.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { FormProvider, useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import { AxiosError } from 'axios';
 import { useQueryClient } from '@tanstack/react-query';
+import { FormProvider, useForm } from 'react-hook-form';
 import {
   PostTaskListRequest,
   useCreateTaskListMutation,
 } from '@/app/lib/tasklist/postTaskList';
-import { AxiosError } from 'axios';
-import { useEffect } from 'react';
-import Modal from '../common/modal/Modal';
-import Button from '../common/button/Button';
-import Input from '../common/input/Input';
+import Input from '@/app/components/common/input/Input';
+import Modal from '@/app/components/common/modal/Modal';
+import Button from '@/app/components/common/button/Button';
 
 interface CreateListModalProps {
   isOpen: boolean;
@@ -23,8 +23,8 @@ export default function CreateListModal({
   onClose,
   groupId,
 }: CreateListModalProps) {
-  const method = useForm<PostTaskListRequest>();
-  const { setError, reset } = method;
+  const methods = useForm<PostTaskListRequest>();
+  const { setError, reset, handleSubmit } = methods;
   const queryClient = useQueryClient();
   const { mutate, isPending } = useCreateTaskListMutation();
 
@@ -34,7 +34,7 @@ export default function CreateListModal({
     }
   }, [isOpen, reset]);
 
-  const handleSubmit = method.handleSubmit((data) => {
+  const onSubmit = handleSubmit((data) => {
     mutate(
       { groupId, name: data.name.trim() },
       {
@@ -57,51 +57,49 @@ export default function CreateListModal({
   });
 
   return (
-    <>
-      <Modal isOpen={isOpen} closeModal={onClose}>
-        <div className="mb-4 flex w-[17.5rem] w-full flex-col gap-4 text-center">
-          <p className="text-lg font-medium">새로운 목록 추가</p>
-          <p className="text-md text-text-secondary">
-            할 일에 대한 목록을 추가하고
-            <br />
-            목록별 할 일을 만들 수 있습니다.
-          </p>
-        </div>
+    <Modal isOpen={isOpen} closeModal={onClose}>
+      <div className="mb-4 flex w-[17.5rem] w-full flex-col gap-4 text-center">
+        <p className="text-lg font-medium">새로운 목록 추가</p>
+        <p className="text-md text-text-secondary">
+          할 일에 대한 목록을 추가하고
+          <br />
+          목록별 할 일을 만들 수 있습니다.
+        </p>
+      </div>
 
-        <FormProvider {...method}>
-          <form className="w-[17.5rem]" onSubmit={handleSubmit}>
-            <div className="flex w-full flex-col gap-6">
-              <Input
-                name="name"
-                title="목록 이름"
-                type="text"
-                placeholder="할 일 목록을 입력해주세요."
-                autoComplete="off"
-                validationRules={{
-                  required: '할 일 목록을 입력해주세요.',
-                  maxLength: {
-                    value: 30,
-                    message: '할 일 목록은 최대 30글자까지 입력 가능합니다.',
-                  },
-                  validate: (value) =>
-                    value.trim() !== '' ||
-                    '할 일 목록은 공백만 입력할 수 없습니다.',
-                }}
-              />
+      <FormProvider {...methods}>
+        <form className="w-[17.5rem]" onSubmit={onSubmit}>
+          <div className="flex w-full flex-col gap-6">
+            <Input
+              name="name"
+              title="목록 이름"
+              type="text"
+              placeholder="할 일 목록을 입력해주세요."
+              autoComplete="off"
+              validationRules={{
+                required: '할 일 목록을 입력해주세요.',
+                maxLength: {
+                  value: 30,
+                  message: '할 일 목록은 최대 30글자까지 입력 가능합니다.',
+                },
+                validate: (value) =>
+                  value.trim() !== '' ||
+                  '할 일 목록은 공백만 입력할 수 없습니다.',
+              }}
+            />
 
-              <Button
-                className="mt-2 w-full text-text-inverse"
-                variant="primary"
-                size="large"
-                type="submit"
-                disabled={isPending}
-              >
-                만들기
-              </Button>
-            </div>
-          </form>
-        </FormProvider>
-      </Modal>
-    </>
+            <Button
+              className="mt-2 w-full text-text-inverse"
+              variant="primary"
+              size="large"
+              type="submit"
+              disabled={isPending}
+            >
+              만들기
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
+    </Modal>
   );
 }
