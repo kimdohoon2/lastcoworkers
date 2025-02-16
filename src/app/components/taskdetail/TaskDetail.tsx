904f45713965e7b32061a5727704752c54d2d480
홍지윤 <h_zhirun@naver.com>
2025-02-16 17:06:33 +0900

    style: 할 일 완료/취소 애니메이션 추가

--- src/app/components/taskdetail/TaskDetail.tsx
+++ src/app/components/taskdetail/TaskDetail.tsx
@@ -272,6 +272,7 @@ function TaskDetail({
               variant={doneAt ? 'cancel' : 'complete'}
               size={doneAt ? 'cancel' : 'complete'}
               onClick={toggleDone}
+              className="transform duration-200"
             >
               <IconCheck />
               {doneAt ? '완료 취소하기' : '완료하기'}
