11facdbbf576e4c8b0c5d11916a230aa88e18359
ksm <hpk5802@github.com>
2025-02-17 18:00:09 +0900

    refactor: 최신 groupData 로드하도록 수정

--- src/app/(routes)/invitation/page.tsx
+++ src/app/(routes)/invitation/page.tsx
@@ -25,7 +25,7 @@ function Page() {
         token,
       });
 
-      queryClient.invalidateQueries({ queryKey: ['group', groupId] });
+      await queryClient.refetchQueries({ queryKey: ['group', groupId] });
       router.push(groupId.toString());
     } catch (error) {
       alert('이미 그룹에 소속된 유저입니다.');
