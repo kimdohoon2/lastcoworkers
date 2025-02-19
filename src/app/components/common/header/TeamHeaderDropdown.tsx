62dcdca2ca2fd44da35bd7f5d4cebb6bfdf427f0
bhoh032019 <bhoh0320@ajou.ac.kr>
2025-02-19 12:12:21 +0900

    feat: 헤더 group Image추가

--- src/app/components/common/header/TeamHeaderDropdown.tsx
+++ src/app/components/common/header/TeamHeaderDropdown.tsx
@@ -28,6 +28,7 @@ export default function TeamHeaderDropdown({
   const currentGroupId = params.teamid ? Number(params.teamid) : null;
 
   let displayGroupName = userData.memberships[0]?.group.name || '팀 생성하기';
+  let displayGroupImage = userData.memberships[0]?.group.image || null;
 
   if (currentGroupId) {
     const membership = userData.memberships.find(
@@ -35,13 +36,26 @@ export default function TeamHeaderDropdown({
     );
     if (membership) {
       displayGroupName = membership.group.name;
+      displayGroupImage = membership.group.image;
     }
   }
 
   return (
     <Dropdown className="relative hidden tablet:block" onClose={closeDropdown}>
       <DropdownToggle onClick={toggleDropdown}>
-        <div className="flex w-[13.625rem] items-center gap-3 truncate">
+        <div className="flex w-[13.625rem] items-center gap-3 truncate px-4">
+          <div className="relative h-8 w-8 flex-shrink-0">
+            {displayGroupImage ? (
+              <Image
+                src={displayGroupImage}
+                alt={displayGroupName}
+                fill
+                className="rounded-md"
+              />
+            ) : (
+              <IconDefaultImage />
+            )}
+          </div>
           <span className="flex-1 truncate text-start">{displayGroupName}</span>
           <IconHeaderCheck
             className={clsx('flex-shrink-0 transition-transform', {
