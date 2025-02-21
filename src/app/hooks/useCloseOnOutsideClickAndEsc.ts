ec6d368d9b0cbd4e304d1d61852eeb2f915f671b
kimdohoon2 <kingyska@nate.com>
2025-02-21 12:41:10 +0000

    style: 이전 코드 주석처리 삭제

--- src/app/hooks/useCloseOnOutsideClickAndEsc.ts
+++ src/app/hooks/useCloseOnOutsideClickAndEsc.ts
@@ -1,25 +1,3 @@
-// import { useEffect } from 'react';
-
-// function useCloseOnOutsideClickAndEsc<T extends HTMLElement>(
-//   ref: React.RefObject<T | null>,
-//   callback: () => void,
-// ) {
-//   useEffect(() => {
-//     const handleClickOutside = (event: MouseEvent) => {
-//       if (ref.current && !ref.current.contains(event.target as Node)) {
-//         callback();
-//       }
-//     };
-
-//     document.addEventListener('click', handleClickOutside);
-//     return () => {
-//       document.removeEventListener('click', handleClickOutside);
-//     };
-//   }, [ref, callback]);
-// }
-
-// export default useCloseOnOutsideClickAndEsc;
-
 import { useEffect } from 'react';
 
 function useCloseOnOutsideClickAndEsc<T extends HTMLElement>(
